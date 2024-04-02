6549f2bb205f4927f1851afb6043f31a51902f7d
Abinash Mohanta <mohantaabinash15@gmail.com>
2024-04-02 12:58:02 +0530

    Portfolio

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,tsjsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
